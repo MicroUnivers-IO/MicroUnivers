3169d9e8eae88dce3e2939d4ce54fdd2135f27db
JulienComolli <comolli.julien@gmail.com>
2022-04-04 22:48:44 +0200

    Mise à jour du script de build

--- game_server/build.js
+++ game_server/build.js
@@ -1,3 +1,5 @@
+const target = "./dist";
+
 require("esbuild").buildSync({
   entryPoints: [
     "./src/server.ts",
@@ -6,5 +8,22 @@ require("esbuild").buildSync({
     "./src/game/Player.ts",
   ],
   bundle: true,
-  outdir: "./dist",
+  outdir: target,
 });
+
+// Deplacer le fichier uWS dans le dossier dist
+
+
+
+const fs = require("fs");
+fs.readdir(target, (err, files) => {
+  files.forEach(file => {
+    if (file.endsWith(".node")) {
+      fs.unlinkSync(target + "/" + file);
+    }
+  });
+});
+
+const uws_target = 'uws_' + process.platform + '_' + process.arch + '_' + process.versions.modules + '.node'; 
+
+fs.copyFileSync("./node_modules/uWebSockets.js/" + uws_target, "./dist/" + uws_target, fs.constants.COPYFILE_EXCL);
