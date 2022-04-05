7e6dd87a2ec93eced66629c756df141af9432ece
JulienComolli <comolli.julien@gmail.com>
2022-04-05 12:36:31 +0200

    Mise à jour du script

--- game_server/src/benchmarks.ts
+++ game_server/src/benchmarks.ts
@@ -4,19 +4,22 @@ import { TICK_SEC } from "./config";
 import { serverLoop, serverLoopBad } from "./loop";
 
 /* -------- Fonction pour benchmark le temps/nb d'exécutions -------- */
-console.time("Temps total : ");
-let totalTicks = 0, nbBenchTicks = 200;
+let startTime = performance.now();
+
+let totalTicks = 0, nbBenchTicks = 1200, timeBetweenTicks = 1000 / TICK_SEC;
 function updateGameBenchMark() {
     if (totalTicks == 0) console.log(`> updateGame benchmark en cours avec : ${TICK_SEC} t/s sur ${nbBenchTicks} ticks. Résultat attendu : ~${nbBenchTicks / TICK_SEC}s `)
     var start = Date.now()
-    while (Date.now() < start.valueOf() + 1000 / TICK_SEC) { }
+    while (Date.now() < start.valueOf() + timeBetweenTicks) { }
 
     totalTicks++;
     process.stdout.cursorTo(0);
     process.stdout.write(`> updateGame n°${totalTicks}/${nbBenchTicks}`);
 
     if (totalTicks == nbBenchTicks) {
-        console.timeEnd("Temps total ");
+        let execTime = performance.now() - startTime;
+        console.log(`\n>> Temps d'exécution : ${execTime/1000}ms`); 
+        console.log(`>> Ticks par seconde executés : ${1000 / (execTime / nbBenchTicks)}`); 
         process.exit(1);
     }
 
