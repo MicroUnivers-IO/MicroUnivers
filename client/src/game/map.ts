131b52f506b8f19c9d20f98d0dd31d955807ca07
Syltenor <raphael.bouchez@etu.u-paris.fr>
2022-04-13 12:22:09 +0200

    4-layered map generation
    Creating the map from a normalized Simplex Noise array.
    Creating texture randomly on campatible array values.

--- client/src/game/map.ts
+++ client/src/game/map.ts
@@ -38,7 +38,7 @@ export class GameMap {
         let bushes = new CompositeRectTileLayer();
         let trees = new CompositeRectTileLayer();
 
-        let mapData = this.perlinGeneration(256, 0.9, 8);
+        let mapData = this.normalizeData(this.perlinGeneration(256, 0.04, 8));
         
         // The tile's x and y position are calculated by multiplying the i and j indexes by the size of a tile.
         // 1 tile = 32px, so (i,j) = (x,y) = (i*32, j*32)
@@ -49,26 +49,20 @@ export class GameMap {
                 let screenX:number =  mapX * this.tileSize;
                 let screenY:number =  mapY * this.tileSize;
 
-                let groundTile:number = Math.abs(Math.floor(mapData[mapX][mapY]/256)*32)+1;
-                let grassTile:number = Math.abs(Math.floor(mapData[mapX][mapY]/this.width)*16)+1;
-                let bushesTile:number = Math.abs(Math.floor(mapData[mapX][mapY]/this.width)*6)+1;
-                let treesTile:number = Math.abs(Math.floor(mapData[mapX][mapY]/this.width)*3)+1;
+                console.log(mapData[mapX][mapY]);
 
-                let tileNb:string = groundTile.toString().padStart(2, '0');
-                let tileTexture:string = 'assets/tileset/field_' + tileNb + '.png';
-                ground.addFrame(tileTexture, screenX, screenY);
+                ground.addFrame(this.randomTile('field', 32), screenX, screenY);
 
-                tileNb = grassTile.toString().padStart(2, '0');
-                tileTexture = 'assets/tileset/grass_' + tileNb + '.png';
-                grass.addFrame(tileTexture, screenX, screenY);
+                if(mapData[mapX][mapY] < 0.2) {
+                    grass.addFrame(this.randomTile('grass', 16), screenX, screenY);
+                }
+                if(mapData[mapX][mapY] < 0.05) {
+                    trees.addFrame(this.randomTile('trees', 3), screenX, screenY);
+                }
+                if(mapData[mapX][mapY] < 0.0001) {
+                    bushes.addFrame(this.randomTile('bush', 6), screenX, screenY);
+                }
 
-                tileNb = bushesTile.toString().padStart(2, '0');
-                tileTexture = 'assets/tileset/bush_' + tileNb + '.png';
-                bushes.addFrame(tileTexture, screenX, screenY);
-
-                tileNb = treesTile.toString().padStart(2, '0');
-                tileTexture = 'assets/tileset/trees_' + tileNb + '.png';
-                trees.addFrame(tileTexture, screenX, screenY);
             }
         }
 
@@ -89,8 +83,38 @@ export class GameMap {
                 frequency: freq,
                 octaves: oct
             });
-
+        
         return res;
+    }
+
+    normalizeData(data):number[][] {
+        let max: number = Math.max.apply(null, data[0]);
+        let min: number = Math.min.apply(null, data[0]);
+
+        for (let i = 1; i < this.height; i++) {
+            let maxTemp:number = Math.max.apply(null, data[0]);
+            let minTemp:number = Math.min.apply(null, data[0]);
+
+            if(maxTemp > max)
+                max = maxTemp;
+            if(minTemp < min)
+                min = minTemp;
+        }
+
+        let res = new Array(this.height);
+        for (let i = 0; i < this.height; i++) {
+            res[i] = new Array(this.width);
+            for (let j = 0; j < this.width; j++) {
+                res[i][j] = (data[i][j] - min) / (max - min);
+            }
+        }
+        return res;
+    }
+
+    randomTile(tileCateg:string, maxTile:number):string {
+        let tileNb:string = (Math.floor(Math.random() * (maxTile-1))+1).toString().padStart(2, '0');
+        let tileTexture:string = 'assets/tileset/'+ tileCateg +'_'+ tileNb +'.png';
+        return tileTexture;
     }
 
     getView():Container {
