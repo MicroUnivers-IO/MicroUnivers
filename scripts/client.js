06ea55c8f3232fba7b5d8a78a50a64a5b3d39524
elkidd <stefan.rvic@gmail.com>
2022-04-14 22:03:09 +0200

    add watch for raph

--- scripts/client.js
+++ scripts/client.js
@@ -2,6 +2,12 @@ require('esbuild').build({
     entryPoints: ['./src/client/app.ts'],
     bundle: true,
     outfile: './src/client/static/bundle-microunivers.js',
+    watch: {
+        onRebuild(error, result) {
+          if (error) console.error('rebuild failed')
+          else console.log('rebuildbuild succeeded:')
+        },
+    }
 }).then(result => {
     console.log("Client build success");
 }).catch(() => {
